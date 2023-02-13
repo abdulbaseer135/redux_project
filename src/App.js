778114f14a433eca479e5282dfcae7e4bd1675f9
Abdul Baseer Khan <abdulbaseerk135@gamil.com>
2023-02-13 07:57:11 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import HeaderContainer from './containers/HeaderContainer';
-import HomeContainer from './containers/HomeContainer'
+import React from "react";
+import "./App.css";
+import HeaderContainer from "./containers/HeaderContainer";
+import HomeContainer from "./containers/HomeContainer";
 function App() {
   return (
     <div className="App">
       <HeaderContainer />
       <HomeContainer />
-     </div>
-  );
+    </div>
+  ); 
 }
 
 export default App;
